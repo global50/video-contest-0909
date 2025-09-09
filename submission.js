f65dc46833d2fafe33801faec5b559d0f4763370
global50 <117812963+global50@users.noreply.github.com>
2025-09-09 13:12:48 +0300

    Updated submission.js

--- submission.js
+++ submission.js
@@ -112,7 +112,7 @@ export function initializeSubmissionForm(userParams = {}) {
       </div>
       <div class="upload-details">
         <span class="upload-speed">0 MB/s</span>
-        <span class="upload-eta">Считаем...</span>
+        <span class="upload-eta">Осталось немного...</span>
       </div>
     `;
     
